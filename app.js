bc681e73055f5c232f49689a8a9c63eefc0d7634
muhammadkashif20 <kashif28@gmail.com>
2024-05-23 00:23:04 +0500

    1 question incomplete

--- app.js
+++ app.js
@@ -66,67 +66,92 @@
     else{
             console.log(ampm='Am');
     }
-}
- */
+} */
 
 
-//Q8
 
-let date=new Date('jan-18-2015')
+
+
+
+// Q8
+
+// {
+//   let now=new Date('31-decemeber-2020')
+//   document.write(`Later Date: ${now}`)
+// }
+
+//Q9
+
+/* let date=new Date('jan-18-2015')
 // console.log(date);
 let ramzanleft=date.getDate()
 // console.log(ramzanleft);
 let convert=date-ramzanleft
-console.log(Math.floor(convert/(1000*3600*24)));
+console.log(Math.floor(convert/(1000*3600*24))); */
 
+//Q10
+/*
+let now=new Date()
+console.log(now);
+let since=new Date('2015')
+console.log(since);
+let seconds=since.getSeconds()
+console.log(seconds);
+let convert=now-since
+// console.log(convert);
+let convSec=Math.floor(convert/(1000));
+document.write(`On reference date ${now}.<br>${convSec}  seconds had passed since beginning of 2015` )
+*/
+//Q11
 
+/* {
+        let now=new Date() 
+        let agoHour=new Date()
+        let ago=agoHour.getHours()
+        let convert=ago-1
+        console.log(convert);
+        let copy=now.toString()
+        let control=copy.slice(0,15)
+        let control1=copy.slice(18)
+        document.write(`Current date: ${now}<br> 1 Hour ago, it was ${control} ${convert}${control1}` )
+} */
 
+//Q12
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* {
+        let now=new Date() 
+        let year=new Date()
+        let Years=year.getFullYear()
+        let convert=Years-100
+        let arr=['sun','mon','tue','wed','thur','Fri','sat']
+        let days=new Date('5-dec-1924')
+        let search=days.getDay()
+        let system=arr[search]
+        let copy=now.toString()
+        let control=copy.slice(3,10)
+        let control1=copy.slice(15)
+        document.write(`Current date: ${now}<br> 100 Years back, it was ${system} ${control} ${convert}${control1}` ) 
+}
+ */
 
 //Q13
 
-// let nws=new Date(prompt('Enter your Birth Date','jan-6-2000'))
-// let convert=nws.getFullYear()
-// document.write(`Your birth year is ${convert} <br>`)
-// let Today=new Date()
-// let age=Today-nws
-// document.write('Your age is: '+Math.floor(age/(1000*3600*24)/365.25));
+/* {
+   let birthYear=new Date(prompt('Enter your Birth Year'));
+   let convert=birthYear.getFullYear()
+   document.write(`Your Age is: ${convert} <br>`)
+   let now=new Date()
+   let convert1=now-birthYear;
+   document.write('Your Birth year is: '+Math.floor(convert1/(1000*3600*24)/365.25))
+       
+   
+} */
 
-
+//Q14
+document.write('<h1>K-Electric Bill</h1>')
+let customerName=(prompt('KE Bill Info:- Enter your Name'));
+document.write(`Customer Name: ${customerName} <br>`)
+let now=new Date()
+let currMon=now.getMonth()
+document.write(`Current Month is: ${currMon}`)
+console.log(currMonth);
